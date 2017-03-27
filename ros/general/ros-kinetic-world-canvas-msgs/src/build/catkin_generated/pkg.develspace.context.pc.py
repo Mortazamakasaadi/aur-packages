be6de148c815b4f536704370b2eb6cd9659abddf
mortazamakasaadi <mortazamakasaadi@gmail.com>
2017-03-28 01:24:40 +0430

    rosCleanUp

--- ros/general/ros-kinetic-world-canvas-msgs/src/build/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/mak/packages/general/ros-kinetic-world-canvas-msgs/src/build/devel/include".split(';') if "/home/mak/packages/general/ros-kinetic-world-canvas-msgs/src/build/devel/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "std_msgs;uuid_msgs;geometry_msgs;std_srvs;message_runtime".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "world_canvas_msgs"
-PROJECT_SPACE_DIR = "/home/mak/packages/general/ros-kinetic-world-canvas-msgs/src/build/devel"
-PROJECT_VERSION = "0.1.0"
